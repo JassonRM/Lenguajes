cff3ddba904ededb1ee8d846d301fa8ce610bd73
Unknown <jassonrm@icloud.com>
2019-05-05 08:40:29 -0600

    Quicksort y avance eliminar

--- Clase 2.rkt
+++ Clase 2.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname |Clase 3|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname |Clase 2|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (define (automovil valores atributos)(
                                       cond
                                            ((null? valores) (list))
@@ -11,12 +11,23 @@
 
 (define (eliminarArbol valor arbol)(
                                     cond
-                                     ((equal? valor (car arbol)) (
+                                     ((equal? valor (valor arbol)) (
                                                                   cond
-                                                                   ((and (null? (cadr arbol)) (null? (caddr arbol))) '())
-                                                                   ((null? (cadr arbol)) (caddr arbol))
-                                                                   (else #f)
+                                                                   ((and (null? (izq arbol)) (null? (der arbol))) '())
+                                                                   ((null? (izq arbol)) (der arbol))
+                                                                   ((null? (der arbol)) (izq arbol))
+                                                                   (else (lista (mayor (izq arbol)) (eliminarArbol (mayor (izq arbol))) (der arbol)))))
 
                                                                    ))
-                                     (else #t)
+                                     ((< valor (valor arbol)) (cons  
                                      ))
+
+(define (valor nodo)(car nodo))
+
+(define (izq nodo)(cadr nodo))
+
+(define (der nodo)(caadr nodo))
+
+(define (mayor nodo)(cond
+                      ((null? (der nodo)) (valor nodo))
+                      (else (mayor nodo))))
