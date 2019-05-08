3b7bbd5f4ca00d9661e9849b12fd70e7c345684c
JassonRM <jassonrm@icloud.com>
2019-05-07 20:32:14 -0600

    Clase 6

---
+++ Clase 6.pl
@@ -0,0 +1,6 @@
+acontecimiento(2012, "Se acabó el mundo").
+acontecimiento(2007, "Anuncian el primer iPhone").
+acontecimiento(2018, "Muere Stephen Hawking").
+
+buscar:-write("Ingrese el año a buscar"),nl,read(Año),acontecimiento(Año, X), write(X).
+
